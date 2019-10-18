0ab74ef64e5ce7b93747f29a8030c89b1be92f13
Andrey Zanevskiy <a.zanevskiy@kakadu.bz>
2019-10-18 03:24:17 +0300

    add DomainUriPrefix to dynamic link library

--- src/DynamicLink.php
+++ src/DynamicLink.php
@@ -8,31 +8,35 @@ class DynamicLink implements \JsonSerializable
 
     private $link;
 
-    private $androidInfo  = null;
+    private $androidInfo = null;
 
-    private $iosInfo  = null;
+    private $iosInfo = null;
 
-    private $navigationInfo  = null;
+    private $navigationInfo = null;
 
-    private $analyticsInfo  = null;
+    private $analyticsInfo = null;
 
-    private $socialMetaTagInfo  = null;
+    private $socialMetaTagInfo = null;
+
+    private $domainUriPrefix = null;
 
     /**
      * DynamicLink constructor.
-     * @param null $link | 	Required if you didn't set a value for the longDynamicLink parameter.
-    The link your app will open. You can specify a URL that your app can handle, typically an app's content/payload, which can initiate app-specific logic (such as crediting the user with a coupon or displaying a welcome screen). This link must be a well-formatted URL, be properly URL-encoded, use the HTTP or HTTPS, and not another Dynamic Link. scheme.
-     * @param null $androidInfo
-     * @param null $iosInfo
+     *
+     * @param null    $link |    Required if you didn't set a value for the longDynamicLink parameter.
+     *                      The link your app will open. You can specify a URL that your app can handle, typically an app's content/payload, which can initiate app-specific logic (such as crediting the user with a coupon or displaying a welcome screen). This link must be a well-formatted URL, be properly URL-encoded, use the HTTP or HTTPS, and not another Dynamic Link. scheme.
+     * @param null    $prefix
+     * @param null    $androidInfo
+     * @param null    $iosInfo
      * @param boolean $enforceRedirectUpdate
-     * @param null $analyticsInfo
-     * @param null $socialMediaTagInfo
-
-    Set the parameter to { "option": "SHORT" } to generate path strings that are only as long as needed to be unique, with a minimum length of 4 characters. Use this method if sensitive information would not be exposed if a short Dynamic Link URL were guessed.
-
-    Omit this parameter or set the parameter to { "option": "UNGUESSABLE" } to shorten the path to an unguessable string. Such strings are created by base62-encoding randomly generated 96-bit numbers, and consist of 17 alphanumeric characters. Use unguessable strings to prevent your Dynamic Links from being crawled, which can potentially expose sensitive information.
+     * @param null    $analyticsInfo
+     * @param null    $socialMediaTagInfo
+     *
+     * Set the parameter to { "option": "SHORT" } to generate path strings that are only as long as needed to be unique, with a minimum length of 4 characters. Use this method if sensitive information would not be exposed if a short Dynamic Link URL were guessed.
+     *
+     * Omit this parameter or set the parameter to { "option": "UNGUESSABLE" } to shorten the path to an unguessable string. Such strings are created by base62-encoding randomly generated 96-bit numbers, and consist of 17 alphanumeric characters. Use unguessable strings to prevent your Dynamic Links from being crawled, which can potentially expose sensitive information.
      */
-    public function __construct($link, $androidInfo = null, $iosInfo = null,
+    public function __construct($link, $prefix = null, $androidInfo = null, $iosInfo = null,
                                 $enforceRedirectUpdate = false, $analyticsInfo = null, $socialMediaTagInfo = null)
     {
         $this->setLink($link);
@@ -41,10 +45,12 @@ class DynamicLink implements \JsonSerializable
         $this->setNavigationInfo($enforceRedirectUpdate);
         $this->setAnalyticsInfo($analyticsInfo);
         $this->setSocialMetaTagInfo($socialMediaTagInfo);
+        $this->setDomainUriPrefix($prefix);
     }
 
     /**
      * @param $analyticsInfo
+     *
      * @return $this
      */
     public function setAnalyticsInfo($analyticsInfo)
@@ -56,61 +62,85 @@ class DynamicLink implements \JsonSerializable
 
     /**
      * @param $android_info
+     *
      * @return $this
      */
     public function setAndroidInfo($android_info)
     {
         $this->androidInfo = $android_info;
+
         return $this;
     }
 
     /**
      * @param $dynamic_link_domain
+     *
      * @return $this
      */
     public function setDynamicLinkDomain($dynamic_link_domain)
     {
         $this->dynamicLinkDomain = $dynamic_link_domain;
+
         return $this;
     }
 
     /**
      * @param $ios_info
+     *
      * @return $this
      */
     public function setIosInfo($ios_info)
     {
         $this->iosInfo = $ios_info;
+
         return $this;
     }
 
     /**
      * @param $link
+     *
      * @return $this
      */
     public function setLink($link)
     {
         $this->link = $link;
+
         return $this;
     }
 
     /**
      * @param int $enableForcedRedirect
+     *
      * @return $this
      */
     public function setNavigationInfo($enableForcedRedirect = 0)
     {
         $this->navigationInfo['enableForcedRedirect'] = $enableForcedRedirect;
+
         return $this;
     }
 
     /**
      * @param $social_meta_tag_info
+     *
      * @return $this
      */
     public function setSocialMetaTagInfo($social_meta_tag_info)
     {
         $this->socialMetaTagInfo = $social_meta_tag_info;
+
+        return $this;
+    }
+
+    /**
+     * @param $prefix
+     *
+     * @return $this
+     */
+    public function setDomainUriPrefix($prefix)
+    {
+        $this->domainUriPrefix = $prefix;
+
         return $this;
     }
 
@@ -171,8 +201,16 @@ class DynamicLink implements \JsonSerializable
     }
 
     /**
+     * @return mixed
+     */
+    public function getDomainUriPrefix()
+    {
+        return $this->domainUriPrefix;
+    }
+
+    /**
      * Specify data which should be serialized to JSON
-     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @link  http://php.net/manual/en/jsonserializable.jsonserialize.php
      * @return mixed data which can be serialized by <b>json_encode</b>,
      * which is a value of any type other than a resource.
      * @since 5.4.0
